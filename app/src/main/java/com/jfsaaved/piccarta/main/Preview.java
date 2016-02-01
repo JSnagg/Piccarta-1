c5f07471e502428f4977547ca0610858757d5d0a
Joseph Snagg <josephsnagg@gmail.com>
2016-01-31 22:42:05 -0500

    Starting to implement camera and preview states
    CHANGES:
    - Copied/pasted camera and preview methods with minor edits
    
    TO DO:
    - Fix it so it works with state manager

---
+++ app/src/main/java/com/jfsaaved/piccarta/main/Preview.java
@@ -0,0 +1,100 @@
+package com.jfsaaved.piccarta.main;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Size;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.IOException;
+import java.util.List;
+
+import static com.jfsaaved.piccarta.main.CameraControl.*;
+
+/**
+ * Created by Joseph on 2016-01-31.
+ */
+abstract class Preview extends ViewGroup implements SurfaceHolder.Callback {
+
+    SurfaceView mSurfaceView;
+    SurfaceHolder mHolder;
+    Camera.Size mPreviewSize;
+    List <Camera.Size> mSupportedPreviewSizes;
+
+    Preview(Context context) {
+        super(context);
+
+        mSurfaceView = new SurfaceView(context);
+        addView(mSurfaceView);
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = mSurfaceView.getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public void setCamera(Camera camera) {
+
+        if (mCamera == camera) { return; }
+
+        stopPreviewAndFreeCamera();
+
+        mCamera = camera;
+
+        if (mCamera != null) {
+            List<Camera.Size> localSizes = mCamera.getParameters().getSupportedPreviewSizes();
+            mSupportedPreviewSizes = localSizes;
+            requestLayout();
+
+            try {
+                mCamera.setPreviewDisplay(mHolder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Important: Call startPreview() to start updating the preview
+            // surface. Preview must be started before you can take a picture.
+            mCamera.startPreview();
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // Now that the size is known, set up the camera parameters and begin
+        // the preview.
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
+        requestLayout();
+        mCamera.setParameters(parameters);
+
+        // Important: Call startPreview() to start updating the preview surface.
+        // Preview must be started before you can take a picture.
+        mCamera.startPreview();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(mPreviewState) {
+            case K_STATE_FROZEN:
+                mCamera.startPreview();
+                mPreviewState = K_STATE_PREVIEW;
+                break;
+
+            default:
+                mCamera.takePicture( null, rawCallback, null);
+                mPreviewState = K_STATE_BUSY;
+        } // switch
+        shutterBtnConfig();
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Surface will be destroyed when we return, so stop the preview.
+        if (mCamera != null) {
+            // Call stopPreview() to stop updating the preview surface.
+            mCamera.stopPreview();
+        }
+    }
+}
+
---
+++ app/src/main/java/com/jfsaaved/piccarta/main/PreviewState.java
@@ -0,0 +1,20 @@
+package com.jfsaaved.piccarta.main;
+
+import states.State;
+import states.StateManager;
+
+/**
+ * Created by Joseph on 2016-01-31.
+ */
+public class PreviewState extends State {
+
+    public PreviewState (StateManager sm) {
+        super(sm);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
+
